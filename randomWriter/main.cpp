0514736214e234e198abd1c893d7cc4d833da3ca
Brendan <braimann2017@pingry.org>
2016-05-31 14:54:07 -0400

    Initial Commit

---
+++ randomWriter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  randomWriter
+//
+//  Created by Brendan Raimann on 5/31/16.
+//  Copyright © 2016 Brendan Raimann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
